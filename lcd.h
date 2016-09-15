20689c56643bc73bf373db00fa985128dcb4adfe
Roman Pletnev <roman.pletnev@promwad.com>
2016-09-15 20:49:59 +0200

    added lcd communications, logo screen

---
+++ lcd.h
@@ -0,0 +1,106 @@
+//LCD Functions Developed by electroSome
+
+//LCD Module Connections
+extern bit LCD_RS;
+extern bit LCD_RW;
+extern bit LCD_EN;
+//#define LCD_PORT P1
+
+
+void lcd_Delay(int a)
+{
+    //unsigned char j;
+    //unsigned char i;
+    int i, j;
+    for (i = 0; i < a; i++) {
+        for(j = 0; j < 100; j++) {
+        }
+    }
+}
+
+void lcd_Cmd(char a)
+{ 
+    LCD_RS = 0;              // => LCD_RS = 0
+    LCD_PORT = a;
+    LCD_EN  = 1;             // => E = 1
+    lcd_Delay(5);
+    LCD_EN  = 0;             // => E = 0
+}
+
+void lcd_Write_Data(char c)
+{
+    LCD_RS = 1;              // => LCD_RS = 1
+    LCD_PORT = c;
+    LCD_EN  = 1;             // => E = 1
+    lcd_Delay(5);
+    LCD_EN  = 0;             // => E = 0
+}
+
+void lcd_Init()
+{
+    LCD_PORT = 0x00;
+    LCD_RS = 0;
+    LCD_RW = 0;
+    lcd_Delay(200);
+    lcd_Clear();
+    ///////////// Reset process from datasheet /////////
+    lcd_Cmd(0x30);
+    lcd_Delay(50);
+    lcd_Cmd(0x30);
+    lcd_Delay(110);
+    lcd_Cmd(0x30);
+    /////////////////////////////////////////////////////
+    lcd_Cmd(0x38);    //function set
+    lcd_Cmd(0x0C);    //display on,cursor off,blink off
+    lcd_Cmd(0x01);    //clear display
+    lcd_Cmd(0x06);    //entry mode, set increment
+    lcd_Delay(50);
+}
+
+lcd_Clear()
+{
+    lcd_Cmd(0x01);
+}
+
+void lcd_Set_Cursor_Pos(char row, char col)
+{
+    if (row == 0)
+        lcd_Cmd(0x80 + col);
+    else if (row == 1)
+        lcd_Cmd(0xC0 + col);
+}
+
+void lcd_Write_String(char *a)
+{
+    int i;
+    for (i = 0; a[i] != '\0'; i++)
+        lcd_Write_Data(a[i]);
+}
+
+void lcd_Load_Custom_Symbol(unsigned char addr, unsigned char *pattern)
+{
+    unsigned char i;
+    lcd_Cmd(0x40 + (addr << 3));
+    for (i = 0; i < 8; i++)
+        lcd_Write_Data(pattern[i]);
+}
+
+void lcd_Cursor_Blink_On()
+{
+    lcd_Cmd(0x0F);
+}
+
+void lcd_Cursor_Blink_Off()
+{
+    lcd_Cmd(0x0C);
+}
+
+void lcd_Shift_Right()
+{
+    lcd_Cmd(0x1C);
+}
+
+void lcd_Shift_Left()
+{
+    lcd_Cmd(0x18);
+}
--- timer.c
+++ timer.c
@@ -1,11 +1,18 @@
 #include "STC12C5620AD.H"
 
 #include <stdio.h>
+#include <stdarg.h>
 #include <stdlib.h>
 #include <intrins.h>
 
+#define LCD_PORT P1
+#include "lcd.h"
+sbit LCD_RS = P2^0;
+sbit LCD_RW = P3^7;
+sbit LCD_EN = P2^1;
+
 #define CPU_FREQ 11059200
-#define T1MS (65536-CPU_FREQ/12/1000)
+#define T1MS (65536 - CPU_FREQ/12/1000)
 
 sbit LED_BLINK = P3^1;
 
@@ -25,6 +32,59 @@ void delay(unsigned int ms)
     }
 }
 
+void lcd_Printf(const char* fmt, ...)
+{
+    char buffer[17];
+    va_list args;
+    
+    va_start(args, fmt);
+    vsprintf (buffer, fmt, args);
+    buffer[16] = '\0';
+    lcd_Write_String(buffer);
+    va_end (args);
+}
+
+void display_logo()
+{
+    const char code m1[8] = {0x1F, 0x18, 0x10, 0x10, 0x10, 0x10, 0x11, 0x11};
+    const char code m2[8] = {0x1F, 0x1F, 0x0F, 0x0F, 0x07, 0x07, 0x03, 0x03};
+    const char code m3[8] = {0x1F, 0x1F, 0x1E, 0x1E, 0x1C, 0x1C, 0x18, 0x18};
+    const char code m4[8] = {0x1F, 0x03, 0x01, 0x01, 0x01, 0x01, 0x11, 0x11};
+    const char code m5[8] = {0x11, 0x11, 0x11, 0x11, 0x11, 0x11, 0x11, 0x1F};
+    const char code m6[8] = {0x11, 0x18, 0x18, 0x1C, 0x1C, 0x1E, 0x1E, 0x1F};
+    const char code m7[8] = {0x11, 0x03, 0x03, 0x07, 0x07, 0x0F, 0x0F, 0x1F};
+    const char code f2[8] = {0x1F, 0x00, 0x00, 0x00, 0xFF, 0xFF, 0xFF, 0x01};
+    
+    lcd_Clear();
+    lcd_Load_Custom_Symbol(0, m1);
+    lcd_Load_Custom_Symbol(1, m2);
+    lcd_Load_Custom_Symbol(2, m3);
+    lcd_Load_Custom_Symbol(3, m4);
+    lcd_Load_Custom_Symbol(4, m5);
+    lcd_Load_Custom_Symbol(5, m6);
+    lcd_Load_Custom_Symbol(6, m7);
+    lcd_Load_Custom_Symbol(7, f2);
+    
+    lcd_Set_Cursor_Pos(0, 0);
+    lcd_Write_Data(0);
+    lcd_Write_Data(1);
+    lcd_Write_Data(2);
+    lcd_Write_Data(3);
+    lcd_Write_Data(0);
+    lcd_Write_Data(7);
+    lcd_Write_Data(255);
+    lcd_Write_String(" MEDICAL ");
+    lcd_Set_Cursor_Pos(1, 0);
+    lcd_Write_Data(4);
+    lcd_Write_Data(5);
+    lcd_Write_Data(6);
+    lcd_Write_Data(4);
+    lcd_Write_Data(4);
+    lcd_Write_Data(255);
+    lcd_Write_Data(255);
+    lcd_Write_String(" F O R T ");
+}
+
 void scan_keyboard()
 {
     
@@ -37,8 +97,11 @@ void main()
     TH0 = T1MS >> 8;
     TR0 = 1;
     ET0 = 1;
-    
     EA = 1;
+    
+    lcd_Init();
+    
+    display_logo();
     
     while (1) {
     }
